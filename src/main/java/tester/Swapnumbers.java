bfeabcc71759cf8b1493b65c3ac11520aff3ce72
sujith <sujitheee206@gmail.com>
2024-12-18 10:42:33 +0530

    add project

---
+++ src/main/java/tester/Swapnumbers.java
@@ -0,0 +1,13 @@
+package tester;
+
+public class Swapnumbers {
+    public static void main(String[] args) {
+//        add two numbers
+        int a=10,b=20;
+        a=a+b;
+        b=a-b;
+        a=a-b;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
